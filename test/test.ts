93ad38ffb7a95f1e678ae8ae0902860942b42480
12pt <alightedness@gmail.com>
2017-04-26 22:35:28 +0100

    Add tests for convert()

--- test/test.ts
+++ test/test.ts
@@ -1,8 +1,7 @@
 import Converter from "../lib/universal";
 import { expect } from "chai";
-//import "mocha";
-
-let conv = new Converter();
+import xlsx = require('xlsx');
+import path = require("path");
 
 describe("colToNumber", function() {
     it("should convert A to 1", function() {
@@ -19,5 +18,22 @@ describe("colToNumber", function() {
 
     it("should convert ZZ to 693", function() {
         expect(Converter.colToNumber("ZQ")).to.equal(693);
+    });
+});
+
+describe("convert", function() {
+    it("should return two objects of equal values with two files containing the same data.", function() {
+        let csv = xlsx.readFile(path.join(__dirname, "./test1.csv"));
+        let excel = xlsx.readFile(path.join(__dirname, "./test1.xlsx"));
+
+        expect(Converter.convert(csv)).to.deep.equal(Converter.convert(excel));
+    });
+
+    it("should return a sheet with a 2D array even if its only one row", function() {
+        let justrow = xlsx.readFile(path.join(__dirname, "./test2.ots"));
+        let expected = {
+            Sheet1: [["a", "b", "c", "d"]]
+        }
+        expect(Converter.convert(justrow)).to.deep.equal(expected);
     });
 });
